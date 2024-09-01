db954cd47c712975e9d248c2e3f6e6a6b2494636
Harshkhosla <harshkhosla9945@gmail.com>
2024-09-02 00:44:55 +0530

    my first backend in ts and some migratiions in sql

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,10 +1,36 @@
-import express from "express"
+import express, { json } from "express"
+import { PrismaClient } from "@prisma/client";
+
+const client=  new PrismaClient();
 
 const app = express();
 
-app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+app.use(express.json());
+app.post("/hooks/catch/:userId/:zapId",async(req,res)=>{
 const userId= req.params.userId;
 const zapId = req.params.zapId;
 
+const body = req.body
+ await client.$transaction(async tx =>{
+   const run = await tx.zapRun.create({
+        data:{
+
+            zapId:zapId,
+            metadata:body
+        }
+    })
+
+    await tx.zapRunOutbox.create({
+        data:{
+            zapRunId:run.id
+        }
+    })
+
+
+ })
+ res.json({
+    message:"webhook recived"
+ })
 
 })
+app.listen(3000)
