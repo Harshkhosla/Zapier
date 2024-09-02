dc93c25b91b97a55f793fc6b2cc2131144366779
Harshkhosla <harshkhosla9945@gmail.com>
2024-09-03 04:25:41 +0530

    wokrinm

--- primary-backend/src/middleware.ts
+++ primary-backend/src/middleware.ts
@@ -2,19 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
 import { JWT_PASS } from "./config";
 
-export function authMiddleware(req:Request , res:Response ,next :NextFunction){
-const token = req.headers.authorization as unknown as string
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization as unknown as string
 
 
-try{
+    try {
 
-    const payload = jwt.verify(token,JWT_PASS)
+        const payload = jwt.verify(token, JWT_PASS)
         // @ts-ignore
-        req.id= payload.id
+        req.id = payload.id
         next();
-}catch(e){
-    return res.status(403).json({
-        message:"You are not logged in"
-    })
-}
+    } catch (e) {
+        return res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
 }
